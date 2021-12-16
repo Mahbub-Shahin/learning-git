324713c24677f0ed8cdd15eaa6b929e8a865163c
Mahbub Shahin <theshahin04@gmail.com>
2021-12-16 23:32:21 +0600

    Added a new mthod called greeting to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public statis void main(String[] args) {
        System.out.println("vs Code is Awesome"); 
+
+       greeting("Mahbub Shahin");
+    }
+
+    public static void greeting(String name) {
+        System.out.println("Welcome " + name);
     }
 }
